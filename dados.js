e4d20dfffad9b0c61768b68f80dd7d78bb6707e5
Lilit Bandeira <lili@Lilits-Macbook-Air.local>
2022-08-20 17:28:30 -0300

    adiciona conteudo dado na aula do dia 20.08.22

---
+++ dados.js
@@ -0,0 +1,35 @@
+// console.log(typeof("Natalia %$#$%  987283478374e Salles"))
+// console.log(typeof('Alane Pimenta'))
+// console.log(typeof(`Letícia Araújo`))
+
+// console.log('1' + 1)
+// let soma = 5
+// console.log("Media é = " + 3)
+// console.log('A soma entre ' + 2 + ' e ' + 3 + ' é igual a ' + 5)
+// console.log(`A soma entre ${2} e ${3} é igual a ${soma}`)
+
+// console.log(typeof(5))
+// console.log(typeof(7.3))
+// console.log(typeof('5'))
+// console.log(typeof(true))
+// console.log(typeof(false))
+
+// let nome 
+// console.log(typeof(nome))
+// nome = "Leinane"
+// console.log(typeof(nome))
+
+var nome = "Jane"
+var idade = "36"
+var signo = "Libra"
+
+var pessoa1 =  {
+  nome: "Jane",
+  idade: 36,
+  signo: "libra"
+}
+
+console.log(typeof(pessoa1))
+
+var nome_da_outra
+
---
+++ variaveis.js
@@ -0,0 +1,19 @@
+// palavra reservada -> var, const ou let
+// identificador -> nomeDaVariável
+// operador de atribuição -> =
+// dado atribuido à variável -> String, number, boolean...
+
+// var nome = "Kaíza"
+// let idade = "18"
+// const signo = "Aquário"
+
+// console.log(Signo)
+
+// nomePessoaQueEuConheciOntem
+
+// let nomeDaPessoa 
+
+// var ci_nasc = "Rio de Janeiro" 
+// var cidadeDeNascimento = "Recife"
+
+// VAR

54f50601d305a9aed40f5326182e18aeb9b12444
hoxas <hoxas@live.com>
2022-08-24 18:26:04 -0300

    Adicionando ex para casa

--- exercicios/alice-almeida/para-casa/index.js
+++ exercicios/alice-almeida/para-casa/index.js
@@ -7,13 +7,47 @@ alto .7 - .799
 muitoAlto 0.8 - 1
 */
 
-let noruega = 0.944
-let qatar = 0.850
-let canada = 0.913
-let brasil = 0.755
-let japao = 0.891
-let vietna = 0.666
-let afeganistao = 0.465
-let camaroes = 0.512
+let noruega = 0.944;
+let qatar = 0.85;
+let canada = 0.913;
+let brasil = 0.755;
+let japao = 0.891;
+let vietna = 0.666;
+let afeganistao = 0.465;
+let camaroes = 0.512;
 
+const listaPaises = {
+  noruega: noruega,
+  qatar: qatar,
+  canada: canada,
+  brasil: brasil,
+  japao: japao,
+  vietna: vietna,
+  afeganistao: afeganistao,
+  camaroes: camaroes,
+};
 
+const idhChecker = (idh) => {
+  if (idh < 0.5) {
+    return "Muito Baixo";
+  } else if (idh < 0.6) {
+    return "Baixo";
+  } else if (idh < 0.7) {
+    return "Médio";
+  } else if (idh < 0.8) {
+    return "Alto";
+  } else {
+    return "Muito Alto";
+  }
+};
+
+console.log("\nOs resultados são : \n");
+Object.keys(listaPaises).forEach((pais) => {
+  console.log(
+    pais.replace(/^\w/, (c) => c.toUpperCase()) +
+      " - " +
+      listaPaises[pais] +
+      " - " +
+      idhChecker(listaPaises[pais])
+  );
+});

f05abf183661280f33c9b1dc7b0fe720914de663
Leinane <leinanesantos24@gmail.com>
2022-08-24 17:01:04 -0300

    Atividade de classificação do IDH

--- exercicios/Leinane-Santos/para-casa/index.js
+++ exercicios/Leinane-Santos/para-casa/index.js
@@ -7,33 +7,36 @@ alto .7 - .799
 muitoAlto 0.8 - 1
 */
 
-//let noruega = 0.944
-//let qatar = 0.850
-//et canada = 0.913
-//let brasil = 0.755
-//let japao = 0.891
-//let vietna = 0.666
-//let afeganistao = 0.465
-//let camaroes = 0.512
+let noruega = 0.944
+let qatar = 0.850
+let canada = 0.913
+let brasil = 0.755
+let japao = 0.891
+let vietna = 0.666
+let afeganistao = 0.465
+let camaroes = 0.512
 
 
-//prompt("Digite o nome do país?")
-//let pais = prompt("Digite o nome do país?")
 
-//let idh = prompt("Digite o IDH")  
-//prompt("Digite o IDH")
+console.log ("idh");
 
-let = idh
+let idh = 0.900;
 
+
+   
 
 if(idh >= 0 && idh <= 0.499) { 
     console.log("Muito Baixo");
-} else if(idh >= 0.5 && idh <= 0.599) { 
+
+} else if(idh >= 0.500 && idh < 0.599) { 
     console.log("Baixo");
-} else if(idh >= 0.6 && idh <= 0.699) {
+
+} else if(idh >= 0.600 && idh < 0.699) {
     console.log("Media");
-} else if( idh <= 0.7 && idh <= 0.799) {
+
+} else if( idh <= 0.700 && idh < 0.799) {
     console.log("Alto");
+
 } else {
     console.log(" Muito Alto"); 
 }

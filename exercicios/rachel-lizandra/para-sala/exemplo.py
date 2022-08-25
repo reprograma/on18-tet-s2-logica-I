f0645dce44ddc24d691cb3ea8cc5b5f1c199b62e
Rachel <rachellizandra@outlook.com>
2022-08-25 13:04:02 -0300

    Adiciona resolução da atividade de JavaScript sobre IDH de alguns países

---
+++ exercicios/rachel-lizandra/para-sala/exemplo.py
@@ -0,0 +1,15 @@
+# start 
+
+print("Digite um número:")
+
+n1 = float(input())
+
+print("Digite outro número:")
+
+n2 = float(input())
+
+media = (n1 + n2) / 2
+
+print("A média entre esses números é: ", media)
+
+# end

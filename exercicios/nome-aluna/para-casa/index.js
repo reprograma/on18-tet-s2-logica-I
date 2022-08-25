121a4273807b735a54ec7da986681a7a01254cd8
kiancaraja <emsmms42@gmail.com>
2022-08-25 00:16:36 -0300

    conclusão de IDH

--- exercicios/nome-aluna/para-casa/index.js
+++ exercicios/nome-aluna/para-casa/index.js
@@ -16,4 +16,18 @@ let vietna = 0.666
 let afeganistao = 0.465
 let camaroes = 0.512
 
+let paises = vietna;
 
+if (paises >= 0 && paises <= 0.499) {
+    console.log('IDH muito baixo');
+} else if (paises >= 0.5 && paises <= 0.599) {
+    console.log('IDH baixo');
+} else if (paises >= 0.6 && paises <= 0.699) {
+    console.log('IDH médio');
+} else if (paises >= 0.7 && paises <= 0.799) {
+    console.log('IDH alto');
+} else if (paises >= 0.8 && paises <= 1) {
+    console.log('IDH muito alto');
+} else {
+    console.log('Digite um IDH válido');
+}

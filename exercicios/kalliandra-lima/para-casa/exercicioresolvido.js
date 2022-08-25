9449c661e0866b1785d9fc44fbf36aad3ae068b8
Kalliandra Lima <kalliandralima@gmail>
2022-08-25 11:00:08 -0300

    git commit -am "New Resolution's problem and fluxogram's addition"

--- exercicios/kalliandra-lima/para-casa/exercicioresolvido.js
+++ exercicios/kalliandra-lima/para-casa/exercicioresolvido.js
@@ -7,18 +7,18 @@ let vietna = 0.666
 let afeganistao = 0.465
 let camaroes = 0.512
 
-const IDH = noruega
+const IDH = camaroes
 
 if (IDH >= 0 && IDH <= 0.0499){
     console.log('IDH muito baixo')
-}else if(IDH >= 0.5 && IDH <= 0.0599 ){
+}else if(IDH >= 0.5 && IDH <= 0.0599){
     console.log( 'IDH baixo')
 }else if(IDH >= 0.6 && IDH <= 0.699){
     console.log('IDH médio')
-}else if (IDH >= 0.7 && IDH <= 0.799) {
+}else if (IDH >= 0.7 && IDH <= 0.799){
     console.log('IDH alto')
-}else if(IDH >=0.8 && IDH <=1) {
-    console.log('IDH muito alto' )
+}else if(IDH >=0.8 && IDH <=1){
+    console.log('IDH muito alto')
 }else{
     console.log('IDH inválido')
 }

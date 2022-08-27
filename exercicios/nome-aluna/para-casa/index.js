bb1e6fef3be70a118c59cc3b0ec16bc0153053cf
Lilit Bandeira <lili@Lilits-Macbook-Air.local>
2022-08-26 21:59:51 -0300

    quarto commit da professora - resolução do esercício dia 26.08.22

--- exercicios/nome-aluna/para-casa/index.js
+++ exercicios/nome-aluna/para-casa/index.js
@@ -7,13 +7,47 @@ alto .7 - .799
 muitoAlto 0.8 - 1
 */
 
-let noruega = 0.944
-let qatar = 0.850
-let canada = 0.913
-let brasil = 0.755
-let japao = 0.891
-let vietna = 0.666
-let afeganistao = 0.465
-let camaroes = 0.512
+const noruega = 0.944
+const qatar = 0.850
+const canada = 0.913
+const brasil = 0.755
+const japao = 0.891
+const vietna = 0.666
+const afeganistao = 0.465
+const camaroes = 0.512
+
+const idh = camaroes
+
+if(idh >=  0 && idh < 0.5) {
+  console.log('IDH é muito baixo')
+} else if (idh >= 0.5 && idh < 0.6) {
+  console.log('IDH é baixo')
+} else if (idh >= 0.6 && idh < 0.7) {
+  console.log('IDH é médio')
+} else if (idh >= 0.7 && idh < 0.8) {
+  console.log('IDH é alto')
+} else if (idh >= 0.8 && idh <= 1) {
+  console.log('IDH é muito alto')
+} else {
+  console.log('IDH inválido')
+}
 
 
+
+//exemplos dados durante as dúvidas:
+
+// const numeroUm = 7
+// let numeroDois = 5
+
+// numeroDois++
+// console.log(numeroUm)
+
+// const aluna = {
+//   nome: "Thaysa",
+//   signo: "gêmeos"
+// }
+
+// console.log(aluna.nome)
+
+// aluna = "Samanta"
+// console.log(aluna.nome)

f64f73c4f703070408e1a3cf62b27590e2eebefe
Camila Alves <camila12.silva@outlook.com>
2022-09-17 00:33:54 -0300

    Lógica camila alves

---
+++ exercicios/camila-alves/para-casa/camilaexcasa.js
@@ -0,0 +1,19 @@
+let IDH = 0.465
+
+if (IDH >= 0 && IDH <= 0.499) 
+{ 
+    console.log("Muito Baixo");
+} else if (IDH >= 0.500 && IDH <= 0.599)
+{
+    console.log("Baixo");
+} else if (IDH >= 0.600 && IDH <=0.699)
+{
+    console.log("Médio");
+} else if (IDH >= 0.700 && IDH <= 0.799)
+{
+    console.log("Alto");
+} else if (IDH >= 0.800 && IDH <=1.00) 
+{
+    console.log("Muito Alto")
+}
+
